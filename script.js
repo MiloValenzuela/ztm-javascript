c6c2d9c2b1515656cd683b93c1f7be2581ede28c
MiloValenzuela <valenzuelagarza@gmail.com>
2025-05-08 16:59:14 -0600

    trying a diferent API

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
 let apiQuotes = [];
 
 // Show new Quote
 function newQuote() {
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-    console.log(quote);
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
 async function getQuotes() {
-    const apiUrl ='https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    const apiUrl ='https://zenquotes.io/api/today';
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.jeson();
